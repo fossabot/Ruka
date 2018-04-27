efe6593f63af9e2ac29615db2b1dc17c2487e33b
Kelvin Kamau <kelvinkamau6808@gmail.com>
2018-04-27 14:11:01 +0300

    Initial commit

---
+++ app/src/main/java/com/example/asus/ruka/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.asus.ruka;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+    static int SPLASH_TIMEOUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               SplashActivity.this.startActivity(new Intent(SplashActivity.this, GameHome.class));
+                SplashActivity.this.finish();
+                   //TODO hide navigation
+                   //TODO Add slider library
+
+            }
+        }, (long) SPLASH_TIMEOUT);
+    }
+}
